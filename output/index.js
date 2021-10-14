d68011086d6436b47ce50807bd659e7783c7591d
Glitch (p5zs) <none>
2021-10-14 15:35:19 +0000

    🍿🐵 Checkpoint ./sketch/index.ts:31736473/687 ./output/index.js:31736473/8233

---
+++ output/index.js
@@ -0,0 +1,10 @@
+/** This function sets up our sketch. */
+export function setup() {
+  createCanvas(500, 500);
+  frameRate(60);
+}
+
+/** This function redraws the sketch multiple times a second. */
+export function draw() {
+  rect(50, 50, 400, 400);
+}
